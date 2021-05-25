3f88c0d014e0b92901b82de71f95fc58f8cd4fe3
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2021-05-25 21:30:00 +0300

    Update for TvNetArticleCommentsTest

--- src/test/java/TvNetArticleCommentsTest.java
+++ src/test/java/TvNetArticleCommentsTest.java
@@ -13,18 +13,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class TvNetArticleCommentsTest {
-    private final By ACCEPT_COOKIE_BTN = By.xpath("");
-    private final By HOME_PAGE_TITLE = By.xpath("");
-    private final By HOME_PAGE_COMMENTS = By.xpath("");
-    private final By HOME_PAGE_ARTICLE = By.tagName("");
+    private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
+    private final By HOME_PAGE_TITLE = By.xpath(".//span[contains(@class, 'list-article__headline')]");
+    private final By HOME_PAGE_COMMENTS = By.xpath(".//span[contains(@class, 'list-article__comment section-font-color')]");
+    private final By HOME_PAGE_ARTICLE = By.tagName("article");
 
-    private final By ARTICLE_PAGE_TITLE = By.xpath("");
-    private final By ARTICLE_PAGE_COMMENTS = By.xpath("");
-    private final By ARTICLE_PAGE_COMMENTS_BTN = By.xpath("");
+    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[@itemprop = 'headline name']");
+    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//span[contains(@class, 'article-share__item--count')]");
+    private final By ARTICLE_PAGE_COMMENTS_BTN = By.xpath(".//img[@src = '/v5/img/icons/comment-v2.svg']");
 
-    private final By COMMENT_PAGE_TITLE = By.xpath("");
-    private final By COMMENT_REGISTRATED_USERS_PAGE_COMMENTS = By.xpath("");
-    private final By COMMENT_ANONIM_USERS_PAGE_COMMENTS = By.xpath("");
+    private final By COMMENT_PAGE_TITLE = By.xpath(".//h1[@class = 'article-headline']");
+    private final By COMMENT_PAGE_COMMENTS = By.xpath(".//span[contains(@class, 'article-comments-heading__count')]");
 
     private final Logger LOGGER = LogManager.getLogger(DelfiArticleCommentsTest2.class);
 
@@ -42,7 +41,7 @@ public class TvNetArticleCommentsTest {
         driver.manage().window().maximize();
 
         LOGGER.info("Opening Home Page");
-        driver.get("http://delfi.lv");
+        driver.get("https://www.tvnet.lv/");
 
         //---------------------------HOME PAGE-----------------------------------------------------------
         WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -69,7 +68,7 @@ public class TvNetArticleCommentsTest {
         int articlePageCommentsCount = getCommentsCount(ARTICLE_PAGE_COMMENTS);
         LOGGER.info("Title is: " + articlePageTitle + "and comments count is: " + articlePageCommentsCount);
 
-        Assertions.assertEquals(homePageTitle, articlePageTitle, "Wrong title!");
+        Assertions.assertEquals(homePageTitle, articlePageTitle + " " + "(" + homePageCommentsCount + ")", "Wrong title!");
         Assertions.assertEquals(homePageCommentsCount, articlePageCommentsCount, "Wrong comments count!");
 
         LOGGER.info("Opening comments page");
@@ -77,11 +76,10 @@ public class TvNetArticleCommentsTest {
         //-------------------------COMMENTS PAGE---------------------------------------------------------
         LOGGER.info("Getting article title and comments count");
         String commentPageTitle = driver.findElement(COMMENT_PAGE_TITLE).getText();
-        int commentPageRegistratedUsersCommentCount = getCommentsCount(COMMENT_REGISTRATED_USERS_PAGE_COMMENTS);
-        int commentPageAnonimUsersCommentCount = getCommentsCount(COMMENT_ANONIM_USERS_PAGE_COMMENTS);
+        int commentPageCommentCount = getCommentsCount(COMMENT_PAGE_COMMENTS);
 
-        Assertions.assertEquals(homePageTitle, commentPageTitle + " ", "Wrong title!");
-        Assertions.assertEquals(homePageCommentsCount, commentPageRegistratedUsersCommentCount + commentPageAnonimUsersCommentCount, "Wrong comments count!");
+        Assertions.assertEquals(homePageTitle, commentPageTitle + " " + "(" + homePageCommentsCount + ")", "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, commentPageCommentCount, "Wrong comments count!");
 
     }
 
@@ -89,7 +87,7 @@ public class TvNetArticleCommentsTest {
         int commentsCount = 0;
 
         if (!driver.findElements(locator).isEmpty()) {
-            commentsCount = removeBrackets(driver.findElement(locator));
+            commentsCount = parseInt(driver.findElement(locator));
         }
 
         return commentsCount;
@@ -108,6 +106,10 @@ public class TvNetArticleCommentsTest {
     private int removeBrackets(WebElement we) {
         String commentsCountText = we.getText();
         commentsCountText = commentsCountText.substring(1, commentsCountText.length() - 1); // (36) -> 36 (String)
+        return Integer.parseInt(commentsCountText); // 36 (String) -> 36 (int)
+    }
+    private int parseInt(WebElement we) {
+        String commentsCountText = we.getText();
         return Integer.parseInt(commentsCountText); // 36 (String) -> 36 (int)
     }
 
